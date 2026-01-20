b7174b57c9de94da9b51962eb64f0843cc23b3dd
ahmethamdiozen <hamdiahmetozen@gmail.com>
2026-01-21 01:57:20 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get("/")
+def ana_sayfa():
+    return {"mesaj": "Selam! İlk endpoint çalışıyor"}
+
+@app.get("/selamla/{isim}")
+def selamla(isim: str):
+    return {"mesaj": f"Merhaba {isim}, FastAPI'a hoş geldin"}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+class Urun(BaseModel):
+    isim: str
+    fiyat: float
+    stokta_mi: bool = True
+
+
+@app.post("/urunler")
+def urun_ekle(yeni_urun: Urun):
+    toplam_fiyat_kdvli = yeni_urun.fiyat * 1.2
+
+    return {
+        "mesaj": f"{yeni_urun.isim} başarıyla eklendi",
+        "fiyat": toplam_fiyat_kdvli
+    }
---
+++ upload.py
@@ -0,0 +1,51 @@
+import os
+import shutil
+from fastapi import FastAPI, UploadFile, HTTPException, File, status
+
+app = FastAPI()
+
+UPLOAD_DIR = "uploads"
+if not os.path.exists(UPLOAD_DIR):
+    os.makedirs(UPLOAD_DIR)
+
+
+@app.post("/upload")
+async def dosya_yukle(file: UploadFile = File(...)):
+    
+    # Dosya gönderilmiş mi?
+    if file is None or file.filename == "":
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Lütfen bir dosya seçiniz."
+        )
+    
+    if not file.filename.lower().endswith("pdf"):
+        raise HTTPException(
+            status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            detail= "Geçersiz dosya formatı. Sadece PDF kabul edilir."
+        )
+    
+    # Sadece PDF kabul et.
+    if file.content_type != "application/pdf":
+        raise HTTPException(status_code=400, detail="Sadece PDF kabul edilir.")
+
+    try:
+    # Yüklenen yer + dosya adı ile path belirle
+        dosya_yolu = os.path.join(UPLOAD_DIR, file.filename)
+    # Stream mantığı ile diske kayıt.
+        with open(dosya_yolu, "wb") as buffer:
+            #file.file = file-like objecttir.
+            shutil.copyfileobj(file.file, buffer)
+    
+    except Exception as e:
+        #Beklenmedik hatada burası çalışsın. 
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Dosya kaydedilirken sunucu tarafında bir hata oluştu: {str(e)}"
+        )
+
+    return {
+        "dosya_adi": file.filename,
+        "icerik_turu": file.content_type,
+        "mesaj": "Dosya başarıyla kaydedildi."
+    }
